ef64202afa4eb93e603a082da94fa18dc8c8b1b8
lalit sachdeva <lalit sachdeva>
2016-11-20 13:12:13 +0530

    updated for input group addon

--- public/app/shared/core/core.directive.js
+++ public/app/shared/core/core.directive.js
@@ -35,6 +35,16 @@
             input.popover('hide');
           }
         }
+
+				$(window).off("resize").on("resize", function() {
+				    $(".popover").each(function() {
+				        var popover = $(this);
+				        if (popover.is(":visible")) {
+				            var ctrl = $(popover.context);
+				            ctrl.popover('show');
+				        }
+				    });
+				});
 			}
 		}
 })();
